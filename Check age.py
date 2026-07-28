d6a2c842a9628f9ec25b7df6804408aaaac2c006
riddhijaincodingal-star <riddhijain.codingal@gmail.com>
2026-07-28 19:22:38 +0530

    Add files via upload

---
+++ Check age.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age "))
+if(age>=10):
+    if(age<=20):
+        print("Your age is",age,",which is between ages 10 and 20")
+    else:
+        print("Sorry,you're age is not between the age of 10 and 20")
+else:
+    print("Sorry,you're age is not between the age of 10 and 20")
+    #end
