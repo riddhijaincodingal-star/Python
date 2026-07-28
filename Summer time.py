55a8b3becabcf2628a139f6190de474f9f07b80a
riddhijaincodingal-star <riddhijain.codingal@gmail.com>
2026-07-28 13:35:00 +0530

    Add files via upload

---
+++ Summer time.py
@@ -0,0 +1,6 @@
+temp=float(input("Enter the temprature in your surroundings "))
+if(temp<24):
+    print("You should wear cold clothes")
+else:
+    print("You should wear light clothes ")
+    #end
