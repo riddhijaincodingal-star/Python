e3e0981b0ae504e3e0a2c197f70ce0cb656e5244
riddhijaincodingal-star <riddhijain.codingal@gmail.com>
2026-07-28 19:33:15 +0530

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,17 @@
+for i in range(1,6,1):
+   for j in range(1,6,1):
+      print(j,end= " ")
+      
+   print()        # i=1  j=1 2 3 4 5  i=2  j=1 2 3 4 5  i=3   j 1 2 3 4 5   i=4  j 1 2 3 4 5 i=5  j  1 2 3 4 5  i=6
+   
+   
+i=1
+while i<=5:
+   j=1
+   while j<=5:
+      print(j, end=" ")  
+      j=j+1
+   i=i+1
+   print()    
+
+    
