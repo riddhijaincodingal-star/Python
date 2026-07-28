ca981b3386205a06dee55c462ae87573c896ccff
riddhijaincodingal-star <riddhijain.codingal@gmail.com>
2026-07-28 16:44:17 +0530

    Add files via upload

---
+++ Number swapping.py
@@ -0,0 +1,9 @@
+a=(input("Enter a number to swap(a) "))
+b=(input("Enter a second number to swap(b) "))
+c=(input("Enter a third number to swap(c) "))
+temp=a
+a=b
+b=c
+c=temp
+print("a,b and c are now",a,",",b,",",c)
+#end
